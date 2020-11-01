4ff872dc540fc69cf80c096435eea47bc8c557a1
Guilherme Dimas da Rosa <guilhermedimasrosa@gmail.com>
2020-11-01 18:10:03 -0300

    Updates

---
+++ precache-manifest.d66bd779218a288d00359d2be1f2679d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774f5f6be96cbc5fce07b261e072e95f",
+    "url": "/reatividade-no-pages/index.html"
+  },
+  {
+    "revision": "5951483919f6e192ba43",
+    "url": "/reatividade-no-pages/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b7a77ec99a481e68946",
+    "url": "/reatividade-no-pages/static/js/2.593b09c5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reatividade-no-pages/static/js/2.593b09c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5951483919f6e192ba43",
+    "url": "/reatividade-no-pages/static/js/main.4c51867c.chunk.js"
+  },
+  {
+    "revision": "342dc3eda54e3e03a2ec",
+    "url": "/reatividade-no-pages/static/js/runtime-main.e57c6b40.js"
+  }
+]);
